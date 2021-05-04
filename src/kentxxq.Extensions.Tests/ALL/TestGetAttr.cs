2fd0290feb656e60c8ac30039c6a462c9365bef2
kentxxq <admin@kentxxq.com>
2021-05-04 21:17:33 +0800

    feat: 获取description的拓展方法。可以用作反射示例

---
+++ src/kentxxq.Extensions.Tests/ALL/TestGetAttr.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel;
+using kentxxq.Extensions.ALL;
+using Xunit;
+
+namespace kentxxq.Extensions.Tests.ALL
+{
+    [Description("展示类")]
+    public class Demo
+    {
+        [Description("主键id")]
+        public int Id { get; set; }
+
+        [Description("用户名")]
+        public string Name;
+    }
+
+    public class TestGetAttr
+    {
+        [Fact]
+        public void TestGetSelfDescription()
+        {
+            Assert.Equal("展示类", typeof(Demo).GetSelfDescriptions()[0]);
+
+            Assert.Equal("主键id", typeof(Demo).GetDescriptionsByPropertyName("Id")[0]);
+
+            Assert.Equal("用户名", typeof(Demo).GetDescriptionsByFieldName("Name")[0]);
+        }
+    }
+}
---
+++ src/kentxxq.Extensions/ALL/GetAttr.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace kentxxq.Extensions.ALL
+{
+    public static class GetAttr
+    {
+        /// <summary>
+        /// 获取自身Description属性值
+        /// </summary>
+        /// <param name="type">任何type类型</param>
+        /// <returns>所有Description值</returns>
+        public static List<string> GetSelfDescriptions(this Type type)
+        {
+            var attrs = (DescriptionAttribute[])type.GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return attrs.Select(a => a.Description).ToList();
+        }
+
+        /// <summary>
+        /// 获取通过字段名称，获取Description值
+        /// </summary>
+        /// <param name="type">任何type类型</param>
+        /// <param name="fieldName">字段名称</param>
+        /// <returns>所有Description值</returns>
+        public static List<string> GetDescriptionsByFieldName(this Type type, string fieldName)
+        {
+            var fields = type.GetFields();
+            foreach (var field in fields)
+            {
+                if (field.Name == fieldName)
+                {
+                    var attrs = (DescriptionAttribute[])field.GetCustomAttributes(typeof(DescriptionAttribute), false);
+                    return attrs.Select(a => a.Description).ToList();
+                }
+            }
+            return new List<string>();
+        }
+
+        /// <summary>
+        /// 获取通过属性名称，获取Description值
+        /// </summary>
+        /// <param name="type">任何type类型</param>
+        /// <param name="propertyName">字段名称</param>
+        /// <returns>所有Description值</returns>
+        public static List<string> GetDescriptionsByPropertyName(this Type type, string propertyName)
+        {
+            var properties = type.GetProperties();
+            foreach (var property in properties)
+            {
+                if (property.Name == propertyName)
+                {
+                    var attrs = (DescriptionAttribute[])property.GetCustomAttributes(typeof(DescriptionAttribute), false);
+                    return attrs.Select(a => a.Description).ToList();
+                }
+            }
+            return new List<string>();
+        }
+
+        /// <summary>
+        /// 获取枚举的Description值
+        /// </summary>
+        /// <param name="enumValue">枚举类里的值</param>
+        /// <returns></returns>
+        public static List<string> GetEnumDescriptions(this Enum enumValue)
+        {
+            var fieldInfo = enumValue.GetType().GetField(enumValue.ToString());
+
+            var descriptionAttributes = (DescriptionAttribute[])fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+            return descriptionAttributes.Select(d => d.Description).ToList();
+        }
+    }
+}
