58a778abb2ce7dfdea95aa0124ccdebddc806f41
kent xxq <805429509@qq.com>
2022-06-10 20:23:08 +0800

    update: 添加默认值ttl

--- src/kentxxq.Utils/Connection.cs
+++ src/kentxxq.Utils/Connection.cs
@@ -13,15 +13,16 @@ public static class Connection
     /// ping测试
     /// </summary>
     /// <param name="ip"></param>
+    /// <param name="ttl">最大跳转数</param>
     /// <param name="timeout">超时时间(毫秒)</param>
     /// <returns></returns>
-    public static bool PingIp(IPAddress ip,int timeout=1000)
+    public static bool PingIp(IPAddress ip,int ttl=255,int timeout=1000)
     {
         var pingSender = new Ping();
         // 32字节数据
         const string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
         var buffer = Encoding.ASCII.GetBytes(data);
-        var pingOption = new PingOptions(64, true);
+        var pingOption = new PingOptions(ttl, true);
         var reply = pingSender.Send(ip, timeout, buffer, pingOption);
         return reply.Status == IPStatus.Success;
     }
@@ -33,7 +34,7 @@ public static class Connection
     /// <param name="ttl">最大跳转数</param>
     /// <param name="timeout">超时时间(毫秒)</param>
     /// <returns></returns>
-    public static PingReply Ping(string url, int ttl=128,int timeout=1000)
+    public static PingReply Ping(string url, int ttl=255,int timeout=1000)
     {
         var ping = new Ping();
         PingOptions pingOptions = new()
@@ -53,15 +54,16 @@ public static class Connection
     /// ping异步测试
     /// </summary>
     /// <param name="ip"></param>
+    /// <param name="ttl">最大跳转数</param>
     /// <param name="timeout">超时时间(毫秒)</param>
     /// <returns></returns>
-    public static async Task<bool> PingIpAsync(IPAddress ip,int timeout=1000)
+    public static async Task<bool> PingIpAsync(IPAddress ip,int ttl=255,int timeout=1000)
     {
         var pingSender = new Ping();
         // 32字节数据
         const string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
         var buffer = Encoding.ASCII.GetBytes(data);
-        var pingOption = new PingOptions(64, true);
+        var pingOption = new PingOptions(ttl, true);
         var reply = await pingSender.SendPingAsync(ip, timeout, buffer, pingOption);
         return reply.Status == IPStatus.Success;
     }
